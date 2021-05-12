c8e09a2fd7e6caeca7d1f9369b9e5fd8788922af
chpark <redgum1004@hanmail.net>
2021-05-12 14:10:39 +0900

    Initial commit

---
+++ src/main/java/com/chpark/park/ParkApplication.java
@@ -0,0 +1,13 @@
+package com.chpark.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkApplication.class, args);
+    }
+
+}
